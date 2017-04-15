741b51d3faecb6c93930e1169011142d4ecc2927
Christine Gierer <beansprout@users.noreply.github.com>
2017-04-14 23:44:16 -0400

    2nd commit

--- src/tree.js
+++ src/tree.js
@@ -9,10 +9,18 @@ class Tree {
     this.children.push(child);
   }
 
-  contains() {
-
+  contains(val) {
+    // should return `true` if the tree or its children the given value.
+    if (this.value === val) return true;
+    this.children.forEach((child) => {
+      if (child.value === val) return true;
+      if (child.children[0]) {
+        return child.contains(val);
+      }
+    });
+    return false;
   }
-
 }
 
+// tree = {value:val, children:[{value:val, children:[]}, {value:val, children:[]}]}
 module.exports = Tree;
