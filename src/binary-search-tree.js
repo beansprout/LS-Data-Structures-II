d7437dafd67a53f28432c8a49c45e89d29ba7d35
Christine Gierer <beansprout@users.noreply.github.com>
2017-04-15 17:15:51 -0400

    binary tree contains works

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -8,17 +8,41 @@ class BinarySearchTree {
   insert(value) {
     const child = new BinarySearchTree(value);
     if (this.value > value) {
-      if (this.left === null) this.left = child;
-      this.left.insert(value);
+      if (this.left === null) {
+        this.left = child;
+      } else {
+        this.left.insert(value);
+      }
     }
     if (this.value < value) {
-      if (this.right === null) this.right = child;
-      this.right.insert(value);
+      if (this.right === null) {
+        this.right = child;
+      } else {
+        this.right.insert(value);
+      }
     }
   }
-  contains() {
-    return true; // TODO
+
+  contains(val) {
+    // should return `true` if the tree or its children the given value.
+    let found = 0;
+    if (this.value === val) {
+      found++;
+    }
+    if (this.right) {
+      if (this.right.contains(val)) {
+        // note to christine.self because of js and closure we don't need to do any n-1 stuff in the recursion like we do in others like SML
+        found++;
+      }
+    }
+    if (this.left) {
+      if (this.left.contains(val)) {
+        found++;
+      }
+    }
+    return (found > 0);
   }
+
   depthFirstForEach() {
     return true; // TODO
   }
